c1eb4669dfc661ef100b21cf83b8f4f685ce1991
komkid <game4670@gmail.com>
2016-07-13 16:09:14 +0700

    หน้า โลโก้ เรียบร้อย

--- app/src/main/java/rus/bamrunglok/komkid/rusrun/MainActivity.java
+++ app/src/main/java/rus/bamrunglok/komkid/rusrun/MainActivity.java
@@ -1,16 +1,32 @@
 package rus.bamrunglok.komkid.rusrun;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+        //Explicit
+    private EditText userEditText, passwordEditText ;
+    private ImageView imageView;
+    private  static  final String urlLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        Picasso.with(this).load(urlLogo).into(imageView);
+
+        //Load Imge from server
     }//Main Method
 
     public void clickSignUpMain(View view){
